fb7f372c3bd4e022ba9b4e104d3d59bd9b637abf
atul kumar <atul.11192@gmail.com>
2020-07-19 15:08:35 +0530

    adding more code for checkout

---
+++ Bill/public/js/main.js
@@ -0,0 +1,10 @@
+checkout=()=>{
+    
+    const billrow=document.getElementById('bill-table').rows;
+    console.log(billrow[0].cells[0].innerText);
+    console.log(billrow[0].cells[1].innerText);
+    console.log(billrow[0].cells[2].innerText);
+    console.log(billrow[0].cells[3].innerText);
+    console.log(billrow[0].cells[4].innerText);
+    
+}
